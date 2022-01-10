f9b23a319459709b7cb701df497a4a6565576bce
li <m3226478166@qq.com>
2022-01-10 16:16:15 +0800

    1

--- lodash/Aby970110-lodash.js
+++ lodash/Aby970110-lodash.js
@@ -1,6 +1,13 @@
-var  Aby970110 = {
+var Aby970110 = {
+  
   chunk: function(array, size) {
-   xxxxxx 
+    const _ = require('lodash');
+    var arr = [1, 2, 3, 4, 5, 6];
+   
+    // size = 1
+    console.log(_.chunk(arr, 1))
+
+   
   }
 
 }
